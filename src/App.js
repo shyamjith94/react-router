140fd6adbfcb660c0628f48483dab4ffb7c033a5
shyamjith94 <shyamjithsmaniyoor@gmail.com>
2022-04-08 17:52:16 +0530

    route with switch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
@@ -13,10 +13,12 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar />
-          <Route exact path='/' component={Home}/>
-          <Route path='/about' component={About} />
-          <Route path='/contact' component={Contact} />
-          <Route path='/posts/:post_id' component={Post} />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+            <Route path='/posts/:post_id' component={Post} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
