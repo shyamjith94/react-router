e67e9a19153b9714352b73f57b51926afe2fb26e
shyamjith94 <shyamjithsmaniyoor@gmail.com>
2022-04-11 17:25:07 +0530

    seperate fun action

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux'
 
-import  {connect} from 'react-redux'
+import deletePost from "../actions/postActions";
+
 class Post extends Component {
     handleClick = () => {
         this.props.deletePost(this.props.post.id)
@@ -38,7 +40,7 @@ const mapStateToProps = (state, ownProps) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         deletePost: (id) => {
-            dispatch({type: 'DELETE_POST', id:id})
+            dispatch(deletePost(id))
         }
     }
 }
