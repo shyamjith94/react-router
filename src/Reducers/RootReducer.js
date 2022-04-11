5732c8cf068136f9982ff7a4a77486bb421a825f
shyamjith94 <shyamjithsmaniyoor@gmail.com>
2022-04-11 17:19:12 +0530

    create and setup reducers wrap component

--- src/Reducers/RootReducer.js
+++ src/Reducers/RootReducer.js
@@ -1,8 +1,21 @@
 const initState = {
-    posts: []
+    posts: [
+        {id:1, title:"redu 1", body:"body 1"},
+        {id:2, title:"redu 2", body:"body 2"},
+        {id:3, title:"redu 3", body:"body 3"},
+    ]
 }
 
-const RootReducer = (state=initState, action) => {
+const RootReducer = (state = initState, action) => {
+    if (action.type === 'DELETE_POST') {
+        let newPost = state.posts.filter(post => {
+            return action.id !== post.id
+        })
+        return {
+            ...state, //kepp all current property and override only post
+            posts:newPost
+        }
+    }
     return state
 }
 
